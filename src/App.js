9bced5b0225ed147a6d8ac6ba8102c652c0943f6
cpflames <cpflames+2@gmail.com>
2024-01-28 22:31:53 +0000

    Enjoy learning chess

--- src/App.js
+++ src/App.js
@@ -5,19 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Onetris!</h1>
+        <h1>Enjoy Learning Chess!</h1>
+        <h2>Chess lesson content, coming soon</h2>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
