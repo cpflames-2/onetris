d50dc7d322b6b98ef30178e850487e7e466e7f13
cpflames <cpflames+2@gmail.com>
2024-01-30 02:03:39 +0000

    I can haz router?

--- src/App.js
+++ src/App.js
@@ -1,79 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import Layout from "./pages/Layout";
+import Home from "./pages/Home.js";
 
-function primesUpto(n) {
-    // Eratosthenes algorithm to find all primes under n
-    var array = [];
-    var upperLimit = Math.sqrt(n);
-    var output = [];
-
-    // Make an array from 2 to (n - 1)
-    for (var i = 0; i < n; i++) {
-        array.push(true);
-    }
-
-    // Remove multiples of primes starting from 2, 3, 5,...
-    for (var i = 2; i <= upperLimit; i++) {
-        if (array[i]) {
-            for (var j = i * i; j < n; j += i) {
-                array[j] = false;
-            }
-        }
-    }
-
-    // All array[i] set to true are primes
-    for (var i = 2; i < n; i++) {
-        if(array[i]) {
-            output.push(i);
-        }
-    }
-
-    return output;
-};
-
-function factorThis(primes, number) {
-  var output = [];
-  var remaining = number;
-  var primeIndex = 0;
-  while(remaining > 1) {
-    var currPrime;
-    if(primeIndex >= primes.length) {
-      currPrime = remaining;
-    } else {
-      currPrime = primes[primeIndex];
-    }
-    console.log({remaining, primeIndex, currPrime});
-    if(remaining % currPrime === 0) {
-      output.push(currPrime);
-      remaining /= currPrime;
-    } else {
-      primeIndex++;
-    }
-  }
-  return output.join(" ");
-}
-
-function App() {
-  var params = new URLSearchParams(window.location.search);
-  var number = params.get('factorme');
-  
-  const primes = primesUpto(Math.sqrt(number));
-  
-  const factoring = factorThis(primes, number);
-  
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Enjoy Learning Chess!</h1>
-        <h2>Chess lesson content, coming soon.</h2>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <h3>{number}</h3>
-      <h3>{factoring}</h3>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
