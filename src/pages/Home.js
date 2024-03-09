eb773c4fd11a106a2dd0ab6a421372300784e447
cpflames <cpflames+2@gmail.com>
2024-03-09 17:18:42 +0000

    Add chess books, and math links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import '../App.css';
 
 export default function Home() {
@@ -15,6 +14,7 @@ export default function Home() {
           <th></th>
           <th>Channels</th>
           <th>Apps</th>
+          <th>Books</th>
         </tr>
         <tr>
           <td>Chess</td>
@@ -30,6 +30,33 @@ export default function Home() {
             <br/>
             ● <a href="https://dragonbox.com/products/learn-chess-with-dragonbox">Learn Chess with DragonBox</a>
             <br/> <small>(formerly "Magnus Kingdom of Chess")</small>
+          </td>
+          <td>
+            ● <a href="https://www.amazon.com/Bobby-Fischer-Teaches-Chess/dp/0553263153">Bobby Fischer Teaches Chess</a>
+            <br/>
+            ● <a href="https://www.amazon.com/How-Win-Chess-Ultimate-Beginners/dp/1984862073">How to Win at Chess</a> by Levy Rozman
+            <br/>
+            ● <a href="https://www.amazon.com/Reassess-Your-Chess/dp/1890085138">Reassess Your Chess</a> by Jeremy Silman
+          </td>
+        </tr>
+        <tr>
+          <td>Math</td>
+          <td>
+            ● <a href="https://www.youtube.com/@3blue1brown">3 blue 1 brown</a>
+            <br/>
+            ● <a href="https://www.youtube.com/user/standupmaths">Stand-Up Maths</a>
+            <br/>
+            ● <a href="https://www.youtube.com/@numberphile">Numberphile</a>
+            <br/>
+            ● <a href="https://www.youtube.com/@Numberblocks">Numberblocks</a>
+          </td>
+          <td>
+            ● <a href="https://brilliant.org/">Brilliant.org</a>
+            <br/>
+            ● <a href="https://www.khanacademy.org/">Khan Academy</a>
+          </td>
+          <td>
+            ● <a href="https://artofproblemsolving.com/store/list/all-products">Art of Problem Solving</a> by Richard Rusczyk
           </td>
         </tr>
       </table>
