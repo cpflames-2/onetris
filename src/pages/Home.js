7428263fbbd93d14896af3bffe15e245e71c8f4c
cpflames <cpflames+2@gmail.com>
2024-11-21 01:20:08 -0800

    Format fixes to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import './Home.css';
 import '../App.css';
 
 export default function Home() {
@@ -15,7 +16,7 @@ export default function Home() {
         In the NWSRS system used in Washington State, the rate of change may be higher for newer players, as they settle into their accurate rating.
       </p>
 
-      <table border="1">
+      <table border="1" style={{ margin: '0 auto' }}>
         <thead>
           <tr>
             <th>RatingΔ</th>
@@ -107,82 +108,97 @@ export default function Home() {
       </table>
 
       <p>
-        RatingΔ is your opponent's rating minus your rating.<br/> 
-        Win Rate is the percentage of games you are expected to win, based on the RatingΔ.<br/>
-        Win, Draw, and Loss are the points you receive for the match.
+        <b>RatingΔ</b> is your opponent's rating minus your rating.<br/> 
+        <b>Win Rate</b> is the percentage of games you are expected to win, based on the RatingΔ.<br/>
+        <b>Win</b>, <b>Draw</b>, and <b>Loss</b> are the points you receive for the match.<br/>
+        <br/>
+        For example, the third row shows that if you're playing someone rated 300 points higher than you,
+        you can expect to win 15% of the time.  If you win, you get +42 points; if you lose, you get -8 points.
+        <br/>
+        If you are instead playing someone rated 300 points lower than you, you can expect to win 85% of the time.
+        If you win, you get +8 points; if you lose, you get -42 points.
+        <br/>
+        The table is based on <a href="https://en.wikipedia.org/wiki/Elo_rating_system">Elo rating system</a> formulas, 
+        which were created to calculate chess ratings. The values in the table are based on a K-Factor of 50, 
+        which is common for elementary school tournaments, to help young players settle into their rating.
+        As players get older and more experienced, the K-Factor will decrease, and the rating changes will be smaller.
       </p>
 
 
-      <h3>What are some good learning resources?</h3>
+      <h3>What are your recommended chess resources?</h3>
+
+      <h4>Chess Books</h4>
+      ▶ <a href="https://www.amazon.com/Bobby-Fischer-Teaches-Chess/dp/0553263153">Bobby Fischer Teaches Chess</a>
+      <br/>
+      ▶ <a href="https://www.amazon.com/How-Win-Chess-Ultimate-Beginners/dp/1984862073">How to Win at Chess</a> by Levy Rozman
+      <br/>
+      ▶ <a href="https://www.amazon.com/Reassess-Your-Chess/dp/1890085138">Reassess Your Chess</a> by Jeremy Silman
+
 
       <h4>Chess Videos</h4>
-      ● <a href="https://www.youtube.com/@ChessKidOfficial">ChessKid / Fun Master Mike</a>
+      ▶ <a href="https://www.youtube.com/@ChessKidOfficial">ChessKid / Fun Master Mike</a>
       <br/>
-      ● <a href="https://www.youtube.com/@GothamChess">Gotham Chess / Levy Rozman</a>
+      ▶ <a href="https://www.youtube.com/@GothamChess">Gotham Chess / Levy Rozman</a>
       <br/>
-      ● <a href="https://www.youtube.com/@eric-rosen">Eric Rosen</a>
+      ▶ <a href="https://www.youtube.com/@eric-rosen">Eric Rosen</a>
       <br/>
-      ● <a href="https://www.youtube.com/@KebuChess">Coach Krishna</a>
+      ▶ <a href="https://www.youtube.com/@KebuChess">Chess Openings / Coach Krishna</a>
 
 
       <h4>Chess Apps</h4>
-      ● <a href="https://www.chesskid.com/app">ChessKid</a>
+      ▶ <a href="https://www.chesskid.com/app">ChessKid</a>
       <br/>
-      ● <a href="https://dragonbox.com/products/learn-chess-with-dragonbox">Learn Chess with DragonBox</a>
+      ▶ <a href="https://dragonbox.com/products/learn-chess-with-dragonbox">Learn Chess with DragonBox</a>
       <br/> <small>(formerly "Magnus Kingdom of Chess")</small>
       <br/>
-      ● <a href="https://play.google.com/store/apps/details?id=com.universis.dinosaurchess">Dinosaur Chess</a>
+      ▶ <a href="https://play.google.com/store/apps/details?id=com.universis.dinosaurchess">Dinosaur Chess</a>
 
-      <h4>Chess Books</h4>
-      ● <a href="https://www.amazon.com/Bobby-Fischer-Teaches-Chess/dp/0553263153">Bobby Fischer Teaches Chess</a>
-      <br/>
-      ● <a href="https://www.amazon.com/How-Win-Chess-Ultimate-Beginners/dp/1984862073">How to Win at Chess</a> by Levy Rozman
-      <br/>
-      ● <a href="https://www.amazon.com/Reassess-Your-Chess/dp/1890085138">Reassess Your Chess</a> by Jeremy Silman
+
+      <h3>What other learning resources do you enjoy?</h3>
 
       <h4>Math Videos</h4>
-      ● <a href="https://www.youtube.com/@3blue1brown">3 blue 1 brown</a>
+      ▶ <a href="https://www.youtube.com/@3blue1brown">3 blue 1 brown</a>
       <br/>
-      ● <a href="https://www.youtube.com/user/standupmaths">Stand-Up Maths</a>
+      ▶ <a href="https://www.youtube.com/user/standupmaths">Stand-Up Maths</a>
       <br/>
-      ● <a href="https://www.youtube.com/@numberphile">Numberphile</a>
+      ▶ <a href="https://www.youtube.com/@numberphile">Numberphile</a>
       <br/>
-      ● <a href="https://www.youtube.com/@Numberblocks">Numberblocks</a>
+      ▶ <a href="https://www.youtube.com/@Numberblocks">Numberblocks</a>
 
       <h4>Math Apps</h4>
-      ● <a href="https://brilliant.org/">Brilliant.org</a>
+      ▶ <a href="https://brilliant.org/">Brilliant.org</a>
       <br/>
-      ● <a href="https://www.khanacademy.org/">Khan Academy</a>
+      ▶ <a href="https://www.khanacademy.org/">Khan Academy</a>
 
       <h4>Science Videos</h4>
-      ● <a href="https://www.youtube.com/@kurzgesagt">Kurzgesagt</a>
+      ▶ <a href="https://www.youtube.com/@kurzgesagt">Kurzgesagt</a>
       <br/>
-      ● <a href="https://www.youtube.com/@NileRed">Nile Red</a>
+      ▶ <a href="https://www.youtube.com/@NileRed">Nile Red</a>
       <br/>
-      ● <a href="https://www.youtube.com/@veritasium">Veritasium</a>
+      ▶ <a href="https://www.youtube.com/@veritasium">Veritasium</a>
       <br/>
-      ● <a href="https://www.youtube.com/@SteveMould">Steve Mould</a>
+      ▶ <a href="https://www.youtube.com/@SteveMould">Steve Mould</a>
       <br/>
-      ● <a href="https://www.youtube.com/@TheActionLab">The Action Lab</a>
+      ▶ <a href="https://www.youtube.com/@TheActionLab">The Action Lab</a>
 
       <h4>Engineering Videos</h4>
-      ● <a href="https://www.youtube.com/@MarkRober">Mark Rober</a>
+      ▶ <a href="https://www.youtube.com/@MarkRober">Mark Rober</a>
       <br/>
-      ● <a href="https://www.youtube.com/@CrunchLabs">Crunch Labs</a>
+      ▶ <a href="https://www.youtube.com/@CrunchLabs">Crunch Labs</a>
       <br/>
-      ● <a href="https://www.youtube.com/@smartereveryday">Smarter Every Day</a>
+      ▶ <a href="https://www.youtube.com/@smartereveryday">Smarter Every Day</a>
 
       <h4>Engineering Apps</h4>
-      ● <a href="https://lightbot.com/">Light Bot</a>
+      ▶ <a href="https://lightbot.com/">Light Bot</a>
       <br/>
-      ● <a href="https://www.avokiddo.com/thinkrolls-app/">Thinkrolls</a>
+      ▶ <a href="https://www.avokiddo.com/thinkrolls-app/">Thinkrolls</a>
 
       <h4>General Learning Videos</h4>
-      ● <a href="https://www.youtube.com/@TEDEd">TED-Ed</a>
+      ▶ <a href="https://www.youtube.com/@TEDEd">TED-Ed</a>
       <br/>
-      ● <a href="https://www.youtube.com/@CGPGrey">CGP Grey</a>
+      ▶ <a href="https://www.youtube.com/@CGPGrey">CGP Grey</a>
       <br/>
-      ● <a href="https://www.youtube.com/@MinuteEarth">Minute Earth</a>
+      ▶ <a href="https://www.youtube.com/@MinuteEarth">Minute Earth</a>
 
     </div>
   );
