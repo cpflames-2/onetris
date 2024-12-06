952ab2c6f2ab4ad0e95aa9d5ed15e3e9c2658f8c
cpflames <cpflames+2@gmail.com>
2024-12-06 13:27:08 -0800

    Logo: Move into public path

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import './TopNav.css';
-import logo from '../assets/images/logo.webp';
 
 export default function TopNav() {
   return (
     <div className="top-nav">
       <Link to="/" className="logo-link">
-        <img src={logo} alt="Logo" className="nav-logo" />
+        <img 
+          src="/images/logo.webp" 
+          alt="Logo" 
+          className="nav-logo" 
+        />
       </Link>
       <Link to="/">Home</Link>
       <Link to="/ratings">Ratings Calculator</Link>
